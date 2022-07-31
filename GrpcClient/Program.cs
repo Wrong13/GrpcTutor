d0781e7a0817cea6f654b478521c2c2a653abab4
wrong <ilak71363@gmail.com>
2022-07-31 14:06:12 +0600

    Сток робит

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Grpc.Net.Client;
+using GrpcClient;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5136");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = "GreeterClient"}
+    );
+    
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key");
+Console.ReadKey();
